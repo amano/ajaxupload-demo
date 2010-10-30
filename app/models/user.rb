cab784a62831263b86e4424d8c4f4930d607b1d1
Mitsuhiro Amano <amano@kray.jp>
2010-10-30 16:19:15 +0900

    s3の設定をuser.rbに配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   # original_filename: config/initializers/paperclip.rb
   has_attached_file :avatar, 
   :storage => :s3,
-  :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+  :s3_credentials => {
+    :access_key_id => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']
+  },
   :bucket => ENV['S3_BUCKET'],
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
   :path => "/dev/ajaxupload-demo/:id/:style_:original_filename",
