fce5e942d3207062cb67abf83ddd67bff5e403ae
Mitsuhiro Amano <amano@kray.jp>
2010-10-30 15:54:02 +0900

    Herokuに対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, 
   :storage => :s3,
   :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-  :bucket => 'capps',
+  :bucket => ENV['S3_BUCKET']
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
   :path => "/tmp/:id/:style_:original_filename",
   :url  => "/tmp/:id/:style/:original_filename"
